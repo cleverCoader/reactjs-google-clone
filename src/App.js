e805a57a5acbc6961998928e9ff138c5a340109e
cleverCoader <adventure.developer45@gmail.com>
2020-09-19 12:37:24 +0530

    complete setup is done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1> amazing-google-clone</h1> */}
+      
+      <Home />
+
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css';
+function Home() {
+    return (
+        <div className="home">
+            <h1> Home page</h1>
+
+            <div className="home_header">
+                <div className="home_headerLeft">
+
+                </div>
+                <div className="home_headerLeft">
+                    
+                </div>
+            </div>
+
+            
+            
+            <div className="home_body">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
